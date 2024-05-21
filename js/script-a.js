64f975428925e33c4599e8a24b407c50f15a96b3
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-21 20:02:13 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -3,7 +3,10 @@ let proximaQuestão = 1;
 const canva = document.querySelector('#canva');
 const inputname = document.querySelector('#name');
 const inputmail = document.querySelector('#mail');
-let dados = [];
+let dados = {
+    nome: '',
+    email: '',
+};
 
 questoes = [`<h1 class="prin__section--h1">Você gostaria de se indentificar?</h1><fieldset class="prin__section--fieldset"><div class="custom-radio"><input id="radio-1" name="question" value="1" type="radio" onclick="querSeIndentificar(1)"><label for="radio-1" class="label">Sim</label></div><div class="custom-radio"><input id="radio-2" name="question" value="0" type="radio" onclick="querSeIndentificar(0)"><label for="radio-2" class="label">Não</label></div></fieldset><button onclick="escreverQuestao()" class="prin__section--button">Próximo.</button>`, ``, ``,]
 
@@ -16,9 +19,9 @@ function voltarParaHome() {
 
 function querSeIndentificar(idf) {
     if (idf == 1) {
-        proximaQuestão = '2';
+        proximaQuestão = 2;
     } else if (idf == 0) {
-        proximaQuestão = '3';
+        proximaQuestão = 3;
     }
     require = true;
     console.log(proximaQuestão);
@@ -29,12 +32,19 @@ function escreverQuestao() {
         canva.innerHTML = `${questoes[proximaQuestão]}`
         require = false
     } else {
-        alert('Marque alguma opção...')
+        alert('Marque alguma opção ou escreva algo...')
     }
 }
 
 function cadDados(perguntas) {
     if (perguntas == 1) {
-        
+        if (inputname.value != '' && inputmail.value != '') {
+            require = true;
+            dados.push(inputname.value);
+            dados.push(inputmail.value);
+        } else if (inputname.value == '' && inputmail.value == '') {
+            require = false;
+        }
+        proximaQuestão = 3;
     }
 }
