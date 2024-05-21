e8da43701ccf42a34afcad395ed53c64a043f253
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-21 19:11:43 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -1,19 +1,7 @@
 let proximaQuestão = 1;
 const canva = document.querySelector('#canva');
 
-questoes = [`<h1>Você gostaria de se indentificar?</h1>
-<fieldset>
-    <div class="custom-radio">
-        <input id="radio-1" name="question" value="1" type="radio" onclick="querSeIndentificar(1)">
-        <label for="radio-1">Sim</label>
-      </div>
-      
-      <div class="custom-radio">
-        <input id="radio-2" name="question" value="0" type="radio" onclick="querSeIndentificar(0)">
-        <label for="radio-2">Não</label>
-      </div>
-</fieldset>
-<button onclick="escreverQuestao()">Próximo.</button>`, ``, ``,]
+questoes = [`<h1 class="prin__section--h1">Você gostaria de se indentificar?</h1><fieldset class="prin__section--fieldset"><div class="custom-radio"><input id="radio-1" name="question" value="1" type="radio" onclick="querSeIndentificar(1)"><label for="radio-1" class="label">Sim</label></div><div class="custom-radio"><input id="radio-2" name="question" value="0" type="radio" onclick="querSeIndentificar(0)"><label for="radio-2" class="label">Não</label></div></fieldset><button onclick="escreverQuestao()" class="prin__section--button">Próximo.</button>`, ``, ``,]
 
 function voltarParaHome() {
     let confirmacao = confirm("Ao clicar em OK, todas seu avanço será perdido. Está ação será irreversivel.");
