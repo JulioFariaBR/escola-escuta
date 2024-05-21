531649cc84ba23832e812a2338afb1e3d351de97
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-21 15:31:59 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -1,7 +1,24 @@
+let proximaQuestão = 'a';
+
+questoes = {
+    a: "",
+    b: "",
+    c: "",
+}
+
 function voltarParaHome() {
     let confirmacao = confirm("Ao clicar em OK, todas seu avanço será perdido. Está ação será irreversivel.");
     if (confirmacao = true) {
         window.location.replace('../../')
     }
 
+}
+
+function querSeIndentificar(idf) {
+    if (idf == 1) {
+        proximaQuestão = 'b';
+    } else if (idf == 0) {
+        proximaQuestão = 'c';
+    }
+    console.log(proximaQuestão);
 }
