64a60171826057f2983b15817f42473c9259a264
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-21 16:09:40 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -18,9 +18,8 @@ questoes = [`<h1>Você gostaria de se indentificar?</h1>
 function voltarParaHome() {
     let confirmacao = confirm("Ao clicar em OK, todas seu avanço será perdido. Está ação será irreversivel.");
     if (confirmacao = true) {
-        window.location.replace('../../')
+        window.location.replace('../../');
     }
-
 }
 
 function querSeIndentificar(idf) {
