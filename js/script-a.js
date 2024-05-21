1322650e83c9de74278ef996ed47b36bef50b7e9
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-21 15:41:23 -0300

    #

--- js/script-a.js
+++ js/script-a.js
@@ -1,10 +1,19 @@
-let proximaQuestão = 'a';
+let proximaQuestão = 1;
+const canva = document.querySelector('#canva');
 
-questoes = {
-    a: "",
-    b: "",
-    c: "",
-}
+questoes = [`<h1>Você gostaria de se indentificar?</h1>
+<fieldset>
+    <div class="custom-radio">
+        <input id="radio-1" name="question" value="1" type="radio" onclick="querSeIndentificar(1)">
+        <label for="radio-1">Sim</label>
+      </div>
+      
+      <div class="custom-radio">
+        <input id="radio-2" name="question" value="0" type="radio" onclick="querSeIndentificar(0)">
+        <label for="radio-2">Não</label>
+      </div>
+</fieldset>
+<button onclick="escreverQuestao()">Próximo.</button>`, ``, ``,]
 
 function voltarParaHome() {
     let confirmacao = confirm("Ao clicar em OK, todas seu avanço será perdido. Está ação será irreversivel.");
@@ -16,9 +25,13 @@ function voltarParaHome() {
 
 function querSeIndentificar(idf) {
     if (idf == 1) {
-        proximaQuestão = 'b';
+        proximaQuestão = '2';
     } else if (idf == 0) {
-        proximaQuestão = 'c';
+        proximaQuestão = '3';
     }
     console.log(proximaQuestão);
+}
+
+function escreverQuestao() {
+    canva.innerHTML = `${questoes[proximaQuestão]}`
 }
