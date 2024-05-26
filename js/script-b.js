7710ffa361426e549bddb0d3969177cce2b54a22
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-25 21:01:39 -0300

    #

---
+++ js/script-b.js
@@ -0,0 +1,17 @@
+const script_do_google = 'https://script.google.com/macros/s/AKfycbyJVIliOevidclzv3C4auzu0yofe0R8Wk3VjGjT65wqMgEhAsF1EO5UNFBemtSeBwSe/exec';
+const dados_do_formulario = {
+    nome: 'Anônimo',
+    email: 'Anônimo',
+    tipo: 'Teste',
+    msn: 'Mensagem sem criatidade',
+};
+
+function bancoDeDados() {
+    fetch(script_do_google, {method: 'POST', body: dados_do_formulario})
+    .then(response => {
+        alert("OK")
+    })
+    .catch(error =>
+        console.error(error)
+    )
+}
