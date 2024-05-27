07d2efa3e37a7061d8b836003a64697ee4f3bf86
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-05-27 20:06:59 -0300

    #

---
+++ js/script-c.js
@@ -0,0 +1,16 @@
+const infos = [
+    {
+        log: 'ADM001',
+        pin: 'ADMJulioFaria001#',
+    },
+    {
+        log: 'ADM002',
+        pin: 'ADMLaraAmorim002#',
+    },
+    {
+        log: 'ADM003',
+        pin: 'ADMLuciaMaldonado003#',
+    },
+]
+
+console.log(infos[1].log);
