dba6125d6781cf849b88b63cb772451ddd198bff
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-06-17 20:27:44 -0300

    #

--- js/script-d.js
+++ js/script-d.js
@@ -40,7 +40,7 @@ function openMsn(id) {
     document.querySelector('#canva-msn').className = 'main dmsn';
     document.querySelector('#canva-msn').innerHTML = `<button class="main__bnt" onclick="closeMsn()">X</button><h1>Detalhes da mensagem:</h1><table><thead><tr class="titulos"><th scope="col">Nome: ${nomeBusca[id]}</th><th scope="col">E-mail: ${emailBusca[id]}</th><th scope="col">Tipo: ${tipoBusca[id]}</th></tr></thead><tbody><tr><th scope="row" colspan="3">${msnBusca[id]}</th></tr></tbody></table>`
     let canvaimpress = document.getElementById('canva-impress');
-    canvaimpress.innerHTML = `${canvaimpress.innerHTML}<div class="impressao">
+    canvaimpress.innerHTML = `<div class="impressao">
         <header class="cab__impressao">
             <img src="../img/assets-11.png" alt="" class="img__impressao">
             <div class="cab--div__impressao">
