9031715815a8a3637c7d0af0b7023c575fa862a0
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2024-06-18 15:40:42 -0300

    #

--- js/script-c.js
+++ js/script-c.js
@@ -36,7 +36,16 @@ function realizarLogin() {
     infos.forEach((e) => {
         if (condition == false) {
             if (e.log == document.querySelector('#login').value && e.pin == document.querySelector('#pass').value) {
-                document.querySelector('#main').innerHTML = `<table><thead><tr class="titulos"><th scope="col">Nome</th><th scope="col">E-mail</th><th scope="col">Tipo</th><th scope="col">Mensagem</th><th scope="col"></th></tr></thead><tbody id="canva-table"></tbody></table>`
+                document.querySelector('#main').innerHTML = `<div id="filtro"><h1>Selecionar Intervalo de Datas</h1>
+                    <form id="dateForm">
+                        <label for="startDate">Data de Início:</label>
+                        <input type="date" id="startDate" name="startDate" required>
+                        <br><br>
+                        <label for="endDate">Data de Fim:</label>
+                        <input type="date" id="endDate" name="endDate" required>
+                        <br><br>
+                        <button type="submit">Filtrar Datas</button>
+                    </form></div><table><thead><tr class="titulos"><th scope="col">Nome</th><th scope="col">E-mail</th><th scope="col">Tipo</th><th scope="col">Mensagem</th><th scope="col"></th></tr></thead><tbody id="canva-table"></tbody></table>`
                 busca();
                 nameuser = e.name
             }
