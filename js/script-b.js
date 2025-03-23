afe9c7463a3d06f6bf684641452cc2bd92601ffb
Julio Cesar S Faria <116515499+JulioFariaBR@users.noreply.github.com>
2025-03-23 15:10:11 -0300

    #

--- js/script-b.js
+++ js/script-b.js
@@ -10,7 +10,7 @@ function tratarDados() {
 
 function bancoDeDados(dados) {
     tratarDados()
-    fetch("https://sclserver.onrender.com#", {
+    fetch("https://sclserver.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dados),
